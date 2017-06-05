46c1b6d4df995a0f3f38f50567c10a4639dfca75
Mahesh Phattesing Masalkar <mahesh.masalkar@imaginea.com>
2017-06-05 20:16:09 +0530

    Added storybook support to the development process

---
+++ src/components/LoginForm.story.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+
+const buttonStyles = {
+  border: '1px solid #eee',
+  borderRadius: 3,
+  backgroundColor: '#FFFFFF',
+  cursor: 'pointer',
+  fontSize: 15,
+  padding: '3px 10px',
+  margin: 10,
+};
+
+const Button = ({ children, onClick }) => (
+  <button
+    style={buttonStyles}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
+Button.propTypes = {
+  children: React.PropTypes.string.isRequired,
+  onClick: React.PropTypes.func,
+};
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
